db5d4ea61d61dd8353149dada92f6a5925f0be67
Leonidas Yopan <leonidasyopan@gmail.com>
2020-05-12 00:06:21 -0300

    focus and blur for input completed.

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -74,8 +74,12 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#FF9000' : '#666360'}
+      />
 
       <TextInput
         ref={inputElementRef}
