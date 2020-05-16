9a028c65ce0459f7706537b58609661139ce29d9
Leonidas Yopan <leonidasyopan@gmail.com>
2020-05-16 17:38:25 -0300

    created Controllers for our routes

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -2,49 +2,22 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import UsersCrontroller from '@modules/users/infra/http/controllers/UsersController';
+import UserAvatarCrontroller from '@modules/users/infra/http/controllers/UserAvatarController';
 
 const userRouter = Router();
 const upload = multer(uploadConfig);
+const usersCrontroller = new UsersCrontroller();
+const userAvatarController = new UserAvatarCrontroller();
 
-userRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const usersRepository = new UsersRepository();
-
-  const createUser = new CreateUserService(usersRepository);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json(user);
-});
+userRouter.post('/', usersCrontroller.create);
 
 userRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const usersRepository = new UsersRepository();
-
-    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 
 export default userRouter;
