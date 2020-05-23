485c995391c26d0649f69708ed1a2ebbc454841f
LeonidasYopan <leonidasyopan@gmail.com>
2020-05-23 17:30:15 -0300

    criada migration do User Tokens

--- backend/src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
+++ backend/src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -1,7 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
@@ -16,6 +15,16 @@ class UserTokensRepository implements IUserTokensRepository {
     const userToken = await this.ormRepository.findOne({
       where: { token },
     });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({
+      user_id,
+    });
+
+    await this.ormRepository.save(userToken);
 
     return userToken;
   }
