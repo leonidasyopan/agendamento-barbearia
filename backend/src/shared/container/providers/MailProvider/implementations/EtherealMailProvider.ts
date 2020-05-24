389126f7c680f030a4e945f4ee8a5aa3e99da790
LeonidasYopan <leonidasyopan@gmail.com>
2020-05-23 23:22:30 -0300

    addded Ethereal Mail Provider

---
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,34 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  private cliente: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.cliente = transporter;
+    });
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.cliente.sendMail({
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperação de Senha.',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
